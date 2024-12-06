585f7855d35bcc6e2996e2e7c01166d335c745fd
Leo <lkeber@gmx.de>
2024-12-06 16:29:33 +0100

    Facial Landmarks (wip)

--- FacialLandmarks.h
+++ FacialLandmarks.h
@@ -30,8 +30,10 @@
  *       - Mouth (green)
  * @note There is no error handling yet.
  */
+//TODO: Include depth
+static std::vector<std::pair<int, int>> GetLandmarks(const std::string& imagePath, const std::string& shapePredictorPath, bool saveResult=false, const std::string& resultPath="") {
+    std::vector<std::pair<int, int>> landmarks;
 
-static void GetLandmarks(const std::string& imagePath, const std::string& shapePredictorPath, bool saveResult=false, const std::string& resultPath="") {
     auto frontal_face_detector = dlib::get_frontal_face_detector();
     dlib::shape_predictor shape_predictor;
     std::cout << shapePredictorPath << std::endl;
@@ -46,15 +48,16 @@ static void GetLandmarks(const std::string& imagePath, const std::string& shapeP
         for (int j = 0; j < shape.num_parts(); ++j) {
             int x = shape.part(j).x();
             int y = shape.part(j).y();
-            if(saveResult){
-                auto color = dlib::rgb_pixel(0, 0, 0);
+            Vector3f coordinates = Vector3f(0, 0, 0);
+            if(saveResult){ //TODO: more readable
+                dlib::rgb_pixel color;
                 if(j < 18){
                     color = dlib::rgb_pixel(255, 0, 0); //jaw
                 }else if(j < 28){
                     color = dlib::rgb_pixel(255, 255, 0); //eyebrows
                 }else if(j < 36){
                     color = dlib::rgb_pixel(0, 0, 255); //nose
-                }else if(j < 49){
+                }else if(j < 48){
                     color = dlib::rgb_pixel(0, 255, 255); //eyes
                 }else{
                     color = dlib::rgb_pixel(0, 255, 0); //mouth
