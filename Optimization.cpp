8d0c78ef480aef036f57511d787a1558b01df231
Leo Keber <leo.keber@tum.de>
2024-12-22 20:18:11 +0100

    Basic Structure Optimization

--- Optimization.cpp
+++ Optimization.cpp
@@ -1,8 +1,49 @@
-//
-// Created by Leo on 21.12.2024.
-//
-
 #include "Optimization.h"
+#include <vector>
+#include <iostream>
 
+
+//Not sure about params and return type yet
+void Optimization::optimizeDenseTerms() {
+    ceres::Problem problem;
+
+    // Mock data
+    std::vector<Eigen::Vector3f> vertices; // Fill with actual data
+    std::vector<double> depths;
+    std::vector<Eigen::Vector3f> normals;
+    std::vector<double> shapeParams(6, 0.0f);        // 6 elements, initialized to 0.0
+    std::vector<double> expressionParams(150, 0.0f);  // prob not floats, but okay for testing purposes
+    //End Mock data
+
+    for (size_t i = 0; i < vertices.size(); ++i) {
+        problem.AddResidualBlock(
+                new ceres::AutoDiffCostFunction<GeometryOptimization, 1, 6, 50>(
+                        new GeometryOptimization(vertices[i], depths[i], normals[i])),
+                nullptr,
+                shapeParams.data(),
+                expressionParams.data()
+                );
+    }
+
+    ceres::Solver::Options options;
+    configureSolver(options);
+    ceres::Solver::Summary summary;
+    ceres::Solve(options, &problem, &summary);
+
+    std::cout << summary.BriefReport() << std::endl;
+}
+
+void Optimization::optimizeSparseTerms() {
+    ceres::Problem problem;
+}
+
+void Optimization::configureSolver(ceres::Solver::Options &options) {
+    options.trust_region_strategy_type = ceres::LEVENBERG_MARQUARDT;
+    options.use_nonmonotonic_steps = false;
+    options.linear_solver_type = ceres::DENSE_QR;
+    options.minimizer_progress_to_stdout = 1;
+    options.max_num_iterations = 1; //maybe make it 100
+    options.num_threads = 8;
+}
 
 
--- Optimization.h
+++ Optimization.h
@@ -1,58 +1,61 @@
-//
-// Created by Leo on 21.12.2024.
-//
-
-
 #ifndef FACE_RECONSTRUCTION_OPTIMIZATION_H
 #define FACE_RECONSTRUCTION_OPTIMIZATION_H
 #include <ceres/ceres.h>
 #include "Eigen.h"
 
+//TODO: THIS SCRIPT IS SUBJECT TO CHANGE!!! Don't look!!! It's ugly!!!
+
 struct GeometryOptimization{
 public:
-    GeometryOptimization(const Eigen::Vector3f& vertex, const Eigen::Vector3f& depth, const Eigen::Vector3f& normal):
-            m_vertex{vertex},
-            m_depth{depth},
-            m_normal{normal}
+    GeometryOptimization(const Eigen::Vector3f& vertex, const float& depth, const Eigen::Vector3f& normal):
+            m_vertex(vertex), m_depth(depth), m_normal(normal)
     {}
 
     template<typename T>
-    bool operator()(const T* const P, T* residuals) const {
-        residuals[0] = 0;
+    bool operator()(const T* const shape, const T* const expression, T* residual) const {
+
+        Eigen::Matrix<T, 3, 1> shape_offset = Eigen::Matrix<T, 3, 1>::Zero();
+        Eigen::Matrix<T, 3, 1> expression_offset = Eigen::Matrix<T, 3, 1>::Zero();
+        auto transformedVertex = m_vertex.cast<T>() + shape_offset + expression_offset;
+
+        //Rotation not yet included. Maybe better to take two params instead of 6 for shape, replacing the floats with vectors?!
+        //because we ignore rotation for now, until we can test it
+        shape_offset.x() += shape[0];
+        shape_offset.y() += shape[1];
+        shape_offset.z() += shape[2];
+
+        for (int i = 0; i < num_expression_params; ++i) {
+            expression_offset.x() += expression[i * 3];
+            expression_offset.y() += expression[i * 3 + 1];
+            expression_offset.z() += expression[i * 3 + 2];
+        }
+        
+        T depth = T(m_depth);
+        T point_to_point_residual = (transformedVertex.z() - depth) * (transformedVertex.z() - depth); //is this square necessary? -> ceres should take care of that?!
+        Eigen::Matrix<T, 3, 1> normal_T = m_normal.cast<T>();
+
+        T point_to_plane_residual = normal_T.dot(transformedVertex - Eigen::Matrix<T, 3, 1>(T(0), T(0), depth));
+
+        residual[0] = point_to_point_residual + point_to_plane_residual;
         return true;
     }
+
 private:
-    Eigen::Vector3f m_vertex;
-    Eigen::Vector3f m_depth;
-    Eigen::Vector3f m_normal;
+    const Eigen::Vector3f m_vertex;
+    const float m_depth;
+    const Eigen::Vector3f m_normal;
+
+    static const int num_shape_params = 6; //0, 1, 2: translation; 3, 4, 5: rotation
+    static const int num_expression_params = 50; //TODO: Change!!!
 };
 
 class Optimization {
-
 public:
+    static void optimizeDenseTerms();
+    static void optimizeSparseTerms();
 
-
-    void optimizeDenseTerms(){
-        ceres::Problem problem;
-        ceres::Solver::Options options;
-        configureSolver(options);
-        ceres::Solver::Summary summary;
-        ceres::Solve(options, &problem, &summary);
-        std::cout << summary.BriefReport() << std::endl;
-        //ceres::Solver::options options;
-        //problem.AddResidualBlock(GeometryOptimization)
-    }
 private:
-
-    void configureSolver(ceres::Solver::Options& options) {
-        options.trust_region_strategy_type = ceres::LEVENBERG_MARQUARDT;
-        options.use_nonmonotonic_steps = false;
-        options.linear_solver_type = ceres::DENSE_QR;
-        options.minimizer_progress_to_stdout = 1;
-        options.max_num_iterations = 1;
-        options.num_threads = 8;
-    }
-
+    static void configureSolver(ceres::Solver::Options& options);
 };
 
 
