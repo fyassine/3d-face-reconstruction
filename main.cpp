926c9c64daeb54cd08d8119870c7c2e30c0a4d6b
Neli Shahapuni <nelishahapuni@gmail.com>
2025-01-09 09:33:24 +0200

    cleanup main()

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <opencv2/opencv.hpp>
+#include <dlib/image_processing.h>
+#include <dlib/opencv.h>
+#include <Eigen/Dense>
+#include <vector>
 
 #include "Eigen.h"
 #include "VirtualSensor.h"
@@ -10,225 +15,12 @@
 #include "Optimization.h"
 #include "Rendering.h"
 #include "BFMParameters.h"
+#include "ProcrustesAligner.h"
 
-#define SHOW_BUNNY_CORRESPONDENCES 1
-
-#define USE_POINT_TO_PLANE	1
-#define USE_LINEAR_ICP		0
-
-#define RUN_SHAPE_ICP		1
-#define RUN_SEQUENCE_ICP	0
-
-void debugCorrespondenceMatching() {
-	// Load the source and target mesh.
-	const std::string filenameSource = std::string("../../../Data/bunny_part2_trans.off");
-	const std::string filenameTarget = std::string("../../../Data/bunny_part1.off");
-
-	SimpleMesh sourceMesh;
-	if (!sourceMesh.loadMesh(filenameSource)) {
-		std::cout << "Mesh file wasn't read successfully." << std::endl;
-		return;
-	}
-
-	SimpleMesh targetMesh;
-	if (!targetMesh.loadMesh(filenameTarget)) {
-		std::cout << "Mesh file wasn't read successfully." << std::endl;
-		return;
-	}
-
-	PointCloud source{ sourceMesh };
-	PointCloud target{ targetMesh };
-    
-    // NELI MARK: - From Excerse 5 - uncomment if needed
-	
-	// Search for matches using FLANN.
-//	std::unique_ptr<NearestNeighborSearch> nearestNeighborSearch = std::make_unique<NearestNeighborSearchFlann>();
-//	nearestNeighborSearch->setMatchingMaxDistance(0.0001f);
-//	nearestNeighborSearch->buildIndex(target.getPoints());
-//	auto matches = nearestNeighborSearch->queryMatches(source.getPoints());
-
-	// Visualize the correspondences with lines.
-	SimpleMesh resultingMesh = SimpleMesh::joinMeshes(sourceMesh, targetMesh, Matrix4f::Identity());
-//	auto sourcePoints = source.getPoints();
-//	auto targetPoints = target.getPoints();
-//
-//	for (unsigned i = 0; i < 100; ++i) { // sourcePoints.size()
-//		const auto match = matches[i];
-//		if (match.idx >= 0) {
-//			const auto& sourcePoint = sourcePoints[i];
-//			const auto& targetPoint = targetPoints[match.idx];
-//			resultingMesh = SimpleMesh::joinMeshes(SimpleMesh::cylinder(sourcePoint, targetPoint, 0.002f, 2, 15), resultingMesh, Matrix4f::Identity());
-//		}
-//	}
-    
-	resultingMesh.writeMesh(std::string("correspondences.off"));
-}
-
-// STEP 1. def alignBunnyWithICP()
-int alignBunnyWithICP() {
-	// Load the source and target mesh.
-	const std::string filenameSource = std::string("../../../Data/bunny_part2_trans.off");
-	const std::string filenameTarget = std::string("../../../Data/bunny_part1.off");
-
-	SimpleMesh sourceMesh;
-	if (!sourceMesh.loadMesh(filenameSource)) {
-		std::cout << "Mesh file wasn't read successfully at location: " << filenameSource << std::endl;
-		return -1;
-	}
-
-	SimpleMesh targetMesh;
-	if (!targetMesh.loadMesh(filenameTarget)) {
-		std::cout << "Mesh file wasn't read successfully at location: " << filenameTarget << std::endl;
-		return -1;
-	}
-    
-    // NELI MARK: - From Excerse 5 - uncomment if needed
-
-	// Estimate the pose from source to target mesh with ICP optimization.
-//	ICPOptimizer* optimizer = nullptr;
-//	if (USE_LINEAR_ICP) {
-//		optimizer = new LinearICPOptimizer();
-//	}
-//	else {
-//		optimizer = new CeresICPOptimizer();
-//	}
-	
-//	optimizer->setMatchingMaxDistance(0.0003f);
-//	if (USE_POINT_TO_PLANE) {
-//		optimizer->usePointToPlaneConstraints(true);
-//		optimizer->setNbOfIterations(10);
-//	}
-//	else {
-//		optimizer->usePointToPlaneConstraints(false);
-//		optimizer->setNbOfIterations(20);
-//	}
-
-	PointCloud source{ sourceMesh };
-	PointCloud target{ targetMesh };
-
-    Matrix4f estimatedPose = Matrix4f::Identity();
-    // NELI MARK: - From Excerse 5 - uncomment if needed
-//	optimizer->estimatePose(source, target, estimatedPose);
-	
-	// Visualize the resulting joined mesh. We add triangulated spheres for point matches.
-	SimpleMesh resultingMesh = SimpleMesh::joinMeshes(sourceMesh, targetMesh, estimatedPose);
-	if (SHOW_BUNNY_CORRESPONDENCES) {
-		for (const auto& sourcePoint : source.getPoints()) {
-			resultingMesh = SimpleMesh::joinMeshes(SimpleMesh::sphere(sourcePoint, 0.001f), resultingMesh, estimatedPose);
-		}
-		for (const auto& targetPoint : target.getPoints()) {
-			resultingMesh = SimpleMesh::joinMeshes(SimpleMesh::sphere(targetPoint, 0.001f, Vector4uc(255, 255, 255, 255)), resultingMesh, Matrix4f::Identity());
-		}
-	}
-	resultingMesh.writeMesh(std::string("../../bunny_icp.off"));
-	std::cout << "Resulting mesh written." << std::endl;
-
-    // NELI MARK: - program crashes when `delete optimizer` is called (mac only issue?)
-//    delete optimizer;
-
-	return 0;
-}
-
-int reconstructRoom() {
-	std::string filenameIn = std::string("../../../Data/rgbd_dataset_freiburg1_xyz/");
-	std::string filenameBaseOut = std::string("mesh_");
-
-	// Load video
-	std::cout << "Initialize virtual sensor..." << std::endl;
-	VirtualSensor sensor;
-	if (!sensor.init(filenameIn)) {
-		std::cout << "Failed to initialize the sensor!\nCheck file path!" << std::endl;
-		return -1;
-	}
-
-	// We store a first frame as a reference frame. All next frames are tracked relatively to the first frame.
-	sensor.processNextFrame();
-	PointCloud target{ sensor.getDepth(), sensor.getDepthIntrinsics(), sensor.getDepthExtrinsics(), sensor.getDepthImageWidth(), sensor.getDepthImageHeight() };
-	
-    // NELI MARK: - From Excerse 5 - uncomment if needed
-
-	// Setup the optimizer.
-//	ICPOptimizer* optimizer = nullptr;
-//	if (USE_LINEAR_ICP) {
-//		optimizer = new LinearICPOptimizer();
-//	}
-//	else {
-//		optimizer = new CeresICPOptimizer();
-//	}
-//
-//	optimizer->setMatchingMaxDistance(0.1f);
-//	if (USE_POINT_TO_PLANE) {
-//		optimizer->usePointToPlaneConstraints(true);
-//		optimizer->setNbOfIterations(10);
-//	}
-//	else {
-//		optimizer->usePointToPlaneConstraints(false);
-//		optimizer->setNbOfIterations(20);
-//	}
-
-	// We store the estimated camera poses.
-	std::vector<Matrix4f> estimatedPoses;
-	Matrix4f currentCameraToWorld = Matrix4f::Identity();
-	estimatedPoses.push_back(currentCameraToWorld.inverse());
-
-	int i = 0;
-	const int iMax = 50;
-	while (sensor.processNextFrame() && i <= iMax) {
-		float* depthMap = sensor.getDepth();
-		Matrix3f depthIntrinsics = sensor.getDepthIntrinsics();
-		Matrix4f depthExtrinsics = sensor.getDepthExtrinsics();
-
-		// Estimate the current camera pose from source to target mesh with ICP optimization.
-		// We downsample the source image to speed up the correspondence matching.
-		PointCloud source{ sensor.getDepth(), sensor.getDepthIntrinsics(), sensor.getDepthExtrinsics(), sensor.getDepthImageWidth(), sensor.getDepthImageHeight(), 8 };
-        
-        // NELI MARK: - From Excerse 5 - uncomment if needed
-//		optimizer->estimatePose(source, target, currentCameraToWorld); // STEP 2. call estimatePose(_,_,_)
-		
-		// Invert the transformation matrix to get the current camera pose.
-		Matrix4f currentCameraPose = currentCameraToWorld.inverse();
-		std::cout << "Current camera pose: " << std::endl << currentCameraPose << std::endl;
-		estimatedPoses.push_back(currentCameraPose);
-
-		if (i % 5 == 0) {
-			// We write out the mesh to file for debugging.
-			SimpleMesh currentDepthMesh{ sensor, currentCameraPose, 0.1f };
-			SimpleMesh currentCameraMesh = SimpleMesh::camera(currentCameraPose, 0.0015f);
-			SimpleMesh resultingMesh = SimpleMesh::joinMeshes(currentDepthMesh, currentCameraMesh, Matrix4f::Identity());
-
-			std::stringstream ss;
-			ss << filenameBaseOut << sensor.getCurrentFrameCnt() << ".off";
-			std::cout << filenameBaseOut << sensor.getCurrentFrameCnt() << ".off" << std::endl;
-			if (!resultingMesh.writeMesh(ss.str())) {
-				std::cout << "Failed to write mesh!\nCheck file path!" << std::endl;
-				return -1;
-			}
-		}
-		
-		i++;
-	}
-
-    // NELI MARK: - From Excerse 5 - uncomment if needed
-//	delete optimizer;
-
-	return 0;
-}
+using namespace Eigen;
+using namespace std;
 
 int main() {
-    // NELI MARK: - Read input.txt file from Data folder
-    const std::string inputFile = std::string("../../Data/input.txt");
-    std::ifstream inFile(inputFile);
-    
-    // write output file to output.txt in Result folder
-    std::ofstream outFile("../../Result/output.txt");
-    if (outFile.is_open()) {
-        std::string line;
-        while (std::getline(inFile, line)) {
-            outFile << line << std::endl;
-        }
-    }
-    // Close output file.
-    outFile.close();
     const std::string imagePath = std::string("../../../Data/face.png");
     const std::string shapePredictorPath = std::string("../../../Data/shape_predictor_68_face_landmarks.dat");
     const std::string outputPath = std::string("../../../Result/output.png");
@@ -245,16 +37,4 @@ int main() {
     //initializeBFM(h5TestFile);
     //readH5File(h5TestFile);
     //readModelPath(h5TestFile);
-
-
-
-    // NELI MARK: - From Excerse 5 - uncomment if needed
-    
-//	int result = 0;
-//	if (RUN_SHAPE_ICP) // shape ICP
-//		result += alignBunnyWithICP(); // STEP 1. call alignBunnyWithICP()
-//	if (RUN_SEQUENCE_ICP) // sequence ICP
-//		result += reconstructRoom();
-//
-//	return result;
 }
