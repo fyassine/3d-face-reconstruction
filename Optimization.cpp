c1059f326a6d03ba34b1408c7386291eb61b529f
Leo Keber <leo.keber@tum.de>
2025-01-30 13:20:35 +0100

    Fixed skipping of vertices

--- Optimization.cpp
+++ Optimization.cpp
@@ -76,17 +76,19 @@ void Optimization::optimize(BfmProperties& bfm, InputImage& inputImage) {
     Eigen::VectorXd shapeParamsD = bfm.shapeParams.cast<double>();
     Eigen::VectorXd expressionParamsD = bfm.expressionParams.cast<double>();
     Eigen::VectorXd colorParamsD = bfm.colorParams.cast<double>();
+    ceres::Problem sparseProblem;
     ceres::Problem problem;
     ceres::Solver::Options options;
     configureSolver(options);
     ceres::Solver::Summary summary;
+    ceres::Solver::Summary sparseSummary;
 
     auto bfmVertices = getVertices(bfm);
     auto landmarks_input_image = inputImage.landmarks;
     auto landmarks_depth_values = inputImage.depthValuesLandmarks;
     for (int i = 0; i < landmarks_input_image.size(); ++i) {
         auto current_landmark = convert2Dto3D(landmarks_input_image[i], landmarks_depth_values[i], inputImage.intrinsics, inputImage.extrinsics);
-        problem.AddResidualBlock(
+        sparseProblem.AddResidualBlock(
                 new ceres::AutoDiffCostFunction<SparseOptimization, 3, 199, 100>(
                         new SparseOptimization(current_landmark, bfmVertices[bfm.landmark_indices[i]], bfm.landmark_indices[i], bfm)
                 ),
@@ -107,7 +109,7 @@ void Optimization::optimize(BfmProperties& bfm, InputImage& inputImage) {
     for (int i = 0; i < 100; ++i) {
         expression_std_dev[i] = std::sqrt(bfm.expressionPcaVariance[i]);
     }
-    problem.AddResidualBlock(
+    sparseProblem.AddResidualBlock(
             new ceres::AutoDiffCostFunction<GeometryRegularizationTerm, 2, 199, 100>(
                     new GeometryRegularizationTerm(identity_std_dev, expression_std_dev)),
             nullptr,
@@ -117,6 +119,54 @@ void Optimization::optimize(BfmProperties& bfm, InputImage& inputImage) {
     //optimizeDenseTerms(bfm, inputImage, problem);
     //regularize(bfm, problem);
 
+    ceres::Solve(options, &sparseProblem, &sparseSummary);
+    //TODO Dense:
+    std::vector<Vector3f> normals = std::vector<Vector3f>(bfmVertices.size(), Vector3f::Zero());
+
+    for (size_t i = 0; i < bfm.triangles.size(); i+=3) {
+        auto triangle0 = bfm.triangles[i];
+        auto triangle1 = bfm.triangles[i+1];
+        auto triangle2 = bfm.triangles[i+2];
+
+        Vector3f faceNormal = (bfmVertices[triangle1] - bfmVertices[triangle0]).cross(bfmVertices[triangle2] - bfmVertices[triangle0]);
+
+        normals[triangle0] += faceNormal;
+        normals[triangle1] += faceNormal;
+        normals[triangle2] += faceNormal;
+    }
+
+    for (size_t i = 0; i < bfmVertices.size(); i++) {
+        normals[i].normalize();
+    }
+
+    int width = 1280;
+    int height = 720;
+
+    // Start Illumination
+
+    for (int i = 0; i < bfmVertices.size(); i+=25) {
+        Eigen::Vector3f vertexBfm = bfmVertices[i];
+        float depthInputImage = getDepthValueFromInputImage(vertexBfm, inputImage.depthValues, width, height, inputImage.intrinsics, inputImage.extrinsics);
+
+        problem.AddResidualBlock(
+                new ceres::AutoDiffCostFunction<GeometryOptimization, 2, 199, 100>(
+                        new GeometryOptimization(bfmVertices[i], depthInputImage, normals[i], bfm, i)
+                ),
+                nullptr,
+                shapeParamsD.data(),
+                expressionParamsD.data()
+        );
+    }
+
+    problem.AddResidualBlock(
+            new ceres::AutoDiffCostFunction<GeometryRegularizationTerm, 2, 199, 100>(
+                    new GeometryRegularizationTerm(identity_std_dev, expression_std_dev)),
+            nullptr,
+            shapeParamsD.data(),
+            expressionParamsD.data()
+    );
+
+    options.max_num_iterations = 100;
     ceres::Solve(options, &problem, &summary);
     bfm.shapeParams = shapeParamsD.cast<float>();
     bfm.expressionParams = expressionParamsD.cast<float>();
