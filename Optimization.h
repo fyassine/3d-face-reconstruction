fdd1cfa37bc38067e3051f0b3bf28a61f239baef
Leo Keber <leo.keber@tum.de>
2025-01-30 12:29:28 +0100

    hmm, something doesnt add up

--- Optimization.h
+++ Optimization.h
@@ -314,10 +314,12 @@ struct GeometryRegularizationTerm {
         // Identity parameters regularization
         for (int i = 0; i < num_identity_params; ++i) {
             reg_energy_geometry += pow(identity_params[i] / T(identity_std_dev[i]), 2);
+            //residual[i] = identity_params[i] * T(sqrt(10));
         }
         // Expression parameters regularization
         for (int i = 0; i < num_expression_params; ++i) {
             reg_energy_expression += pow(expression_params[i] / T(expression_std_dev[i]), 2);
+            //residual[num_identity_params + i] = expression_params[i] * T(sqrt(8));
         }
 
         residual[0] = reg_energy_geometry;
