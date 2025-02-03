ec58c90fa7127e68072136d0bb96c306e41fc735
Leo Keber <leo.keber@tum.de>
2025-02-03 23:33:00 +0100

    Added support for PointClouds

--- ModelConverter.cpp
+++ ModelConverter.cpp
@@ -18,7 +18,7 @@ void ModelConverter::convertToPly(std::vector<Vector3d> vertices, const std::vec
     outFile << "property uchar green" << std::endl;
     outFile << "property uchar blue" << std::endl;
     outFile << "property uchar alpha" << std::endl;
-    outFile << "element face " << faces.size() << std::endl;
+    outFile << "element face " << faces.size() / 3 << std::endl;
     outFile << "property list uchar int vertex_indices" << std::endl;
     outFile << "end_header" << std::endl;
 
@@ -34,6 +34,73 @@ void ModelConverter::convertToPly(std::vector<Vector3d> vertices, const std::vec
 
     for (int i = 0; i < faces.size(); i+=3) {
         outFile << "3 " << faces[i] << " " << faces[i + 1] << " " << faces[i + 2] << std::endl;
+    }
+    outFile.close();
+}
+
+void ModelConverter::convertToPly(std::vector<Vector3d> vertices, const std::string &path) {
+    std::ofstream outFile(RESULT_FOLDER_PATH + path);
+
+    outFile << "ply" << std::endl;
+    outFile << "format ascii 1.0" << std::endl;
+    outFile << "element vertex " << vertices.size() << std::endl;
+    outFile << "property float x" << std::endl;
+    outFile << "property float y" << std::endl;
+    outFile << "property float z" << std::endl;
+    outFile << "property uchar red" << std::endl;
+    outFile << "property uchar green" << std::endl;
+    outFile << "property uchar blue" << std::endl;
+    outFile << "property uchar alpha" << std::endl;
+    outFile << "element face " << 0 << std::endl;
+    outFile << "property list uchar int vertex_indices" << std::endl;
+    outFile << "end_header" << std::endl;
+
+    for (int i = 0; i < vertices.size(); i++) {
+        auto x = vertices[i].x();
+        auto y = vertices[i].y();
+        auto z = vertices[i].z();
+        auto r = 0;
+        auto g = 255;
+        auto b = 0;
+        outFile << x << " " << y << " " << z << " " << r << " "<< g << " "<< b << " 255"<< std::endl;
+    }
+    outFile.close();
+}
+
+void ModelConverter::convertImageToPly(const std::vector<double>& depth, const std::vector<Vector3d>& colorValues, const std::string& path,
+                                       const Matrix3d& intrinsics, const Matrix4d& extrinsics) {
+
+    std::vector<Eigen::Vector2d> pointCloudVertices;
+    for (int i = 0; i < 720; ++i) {
+        for (int j = 0; j < 1280; ++j) {
+            pointCloudVertices.emplace_back(Eigen::Vector2d(j, i));
+        }
+    }
+
+    std::ofstream outFile(RESULT_FOLDER_PATH + path);
+    outFile << "ply" << std::endl;
+    outFile << "format ascii 1.0" << std::endl;
+    outFile << "element vertex " << pointCloudVertices.size() << std::endl;
+    outFile << "property float x" << std::endl;
+    outFile << "property float y" << std::endl;
+    outFile << "property float z" << std::endl;
+    outFile << "property uchar red" << std::endl;
+    outFile << "property uchar green" << std::endl;
+    outFile << "property uchar blue" << std::endl;
+    outFile << "property uchar alpha" << std::endl;
+    outFile << "element face " << 0 << std::endl;
+    outFile << "property list uchar int vertex_indices" << std::endl;
+    outFile << "end_header" << std::endl;
+
+    for (int i = 0; i < pointCloudVertices.size(); i++) {
+        Eigen::Vector3d vertex3D = InputDataExtractor::convert2Dto3D(pointCloudVertices[i], depth[i], intrinsics, extrinsics);
+        auto x = (float) vertex3D.x();
+        auto y = (float) vertex3D.y();
+        auto z = (float) vertex3D.z();
+        auto r = colorValues[i].x() * 255;
+        auto g = colorValues[i].y() * 255;
+        auto b = colorValues[i].z() * 255;
+        outFile << x << " " << y << " " << z << " " << r << " "<< g << " "<< b << " 255"<< std::endl;
     }
     outFile.close();
 }
