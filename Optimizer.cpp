57320cfebb6301c9ab08cdd24b0f1b3a97956f98
Leo Keber <leo.keber@tum.de>
2025-02-03 19:50:51 +0100

    New Sparse Terms

--- Optimizer.cpp
+++ Optimizer.cpp
@@ -1,14 +1,36 @@
 #include "Optimizer.h"
 
-Optimizer::Optimizer(BaselFaceModel *baselFaceModel, SingleInputFrame *singleInputFrame) {
+Optimizer::Optimizer(BaselFaceModel *baselFaceModel, InputData *inputData) {
     m_baselFaceModel = baselFaceModel;
-    m_singleInputFrame = singleInputFrame;
+    m_inputData = inputData;
+    configureSolver();
 }
 
 Optimizer::~Optimizer() = default;
 
 void Optimizer::optimizeSparseTerms() {
+    ceres::Problem problem;
 
+    auto landmark_indices_bfm = m_baselFaceModel->getLandmarkIndices();
+    auto landmarks_input_data = m_inputData->getMCurrentFrame().getMLandmarks();
+    int n = (int) landmark_indices_bfm.size();
+    for (int i = 0; i < n; ++i) {
+        if(landmarks_input_data[i].x() == -1) continue;
+        problem.AddResidualBlock(
+                new ceres::AutoDiffCostFunction<SparseOptimizationCost, 3, 199, 100>(
+                        new SparseOptimizationCost(m_baselFaceModel, landmarks_input_data[i], landmark_indices_bfm[i])
+                ),
+                nullptr,
+                m_baselFaceModel->getShapeParams().data(), //TODO: könnte getter hier ein problem werden?!?
+                m_baselFaceModel->getExpressionParams().data()
+                );
+    }
+
+    ceres::Solver::Summary summary;
+    std::cout << "Sparse Optimization initiated." << std::endl;
+    ceres::Solve(options, &problem, &summary);
+    std::cout << summary.BriefReport() << std::endl;
+    std::cout << "Sparse Optimization finished." << std::endl;
 }
 
 void Optimizer::optimizeDenseGeometryTerm() {
@@ -23,4 +45,15 @@ void Optimizer::optimize() {
     optimizeSparseTerms();
     optimizeDenseGeometryTerm();
     optimizeDenseColorTerm();
+}
+
+void Optimizer::configureSolver() {
+    options.trust_region_strategy_type = ceres::LEVENBERG_MARQUARDT;
+    options.dense_linear_algebra_library_type = ceres::CUDA;
+    options.sparse_linear_algebra_library_type = ceres::CUDA_SPARSE;
+    options.use_nonmonotonic_steps = false; //TODO: Maybe das hier löschen
+    options.linear_solver_type = ceres::DENSE_QR;
+    options.minimizer_progress_to_stdout = true;
+    options.max_num_iterations = 1000;
+    options.num_threads = 24;
 }
--- Optimizer.h
+++ Optimizer.h
@@ -2,19 +2,91 @@
 #define FACE_RECONSTRUCTION_OPTIMIZER_H
 
 #include "BaselFaceModel.h"
-#include "SingleInputFrame.h"
+#include "InputData.h"
+#include <ceres/ceres.h>
+
+#define NUM_SHAPE_PARAMETERS 199
+#define NUM_EXPRESSION_PARAMETERS 100
+#define NUM_COLOR_PARAMETERS 199
 
 class Optimizer {
 public:
-    Optimizer(BaselFaceModel* baselFaceModel, SingleInputFrame* singleInputFrame);
+    Optimizer(BaselFaceModel* baselFaceModel, InputData* inputData);
     ~Optimizer();
     void optimizeSparseTerms();
     void optimizeDenseGeometryTerm();
     void optimizeDenseColorTerm();
     void optimize();
+    void configureSolver();
 private:
     BaselFaceModel* m_baselFaceModel;
-    SingleInputFrame* m_singleInputFrame;
+    InputData* m_inputData;
+    ceres::Solver::Options options;
+};
+
+struct SparseOptimizationCost {
+public:
+    SparseOptimizationCost(BaselFaceModel* baselFaceModel, Vector3d landmark_image, int landmark_bfm_index)
+            : m_baselFaceModel{baselFaceModel}, m_landmark_image{landmark_image}, m_landmark_bfm_index{landmark_bfm_index} {}
+
+    template <typename T>
+    bool operator()(const T* const shape,
+                    const T* const expression,
+                    T* residuals) const {
+
+        auto shapeMean = m_baselFaceModel->getShapeMean();
+        auto expressionMean = m_baselFaceModel->getExpressionMean();
+
+        auto shapePcaBasis = m_baselFaceModel->getShapePcaBasis();
+        auto expressionPcaBasis = m_baselFaceModel->getExpressionPcaBasis();
+
+        auto shapeVariance = m_baselFaceModel->getShapePcaVariance();
+        auto expressionVariance = m_baselFaceModel->getExpressionPcaVariance();
+
+        auto transformationMatrix = m_baselFaceModel->getTransformation();
+
+        Eigen::Matrix<T, 4, 1> offset = Eigen::Matrix<T, 4, 1>(
+                T(shapeMean[m_landmark_bfm_index * 3]) + T(expressionMean[m_landmark_bfm_index * 3]),
+                T(shapeMean[m_landmark_bfm_index * 3 + 1]) + T(expressionMean[m_landmark_bfm_index * 3 + 1]),
+                T(shapeMean[m_landmark_bfm_index * 3 + 2]) + T(expressionMean[m_landmark_bfm_index * 3 + 2]),
+                T(1)
+        );
+
+        for (int i = 0; i < NUM_SHAPE_PARAMETERS; ++i) {
+            int vertex_idx = m_landmark_bfm_index * 3;
+            T param = T(sqrt(shapeVariance[i])) * shape[i];
+            offset += Eigen::Matrix<T, 4, 1>(
+                    param * T(shapePcaBasis(vertex_idx, i)),
+                    param * T(shapePcaBasis(vertex_idx + 1, i)),
+                    param * T(shapePcaBasis(vertex_idx + 2, i)),
+                    T(0)
+            );
+        }
+
+        for (int i = 0; i < NUM_EXPRESSION_PARAMETERS; ++i) {
+            int vertex_idx = m_landmark_bfm_index * 3;
+            T param = T(sqrt(expressionVariance[i])) * expression[i];
+            offset += Eigen::Matrix<T, 4, 1>(
+                    param * T(expressionPcaBasis(vertex_idx, i)),
+                    param * T(expressionPcaBasis(vertex_idx + 1, i)),
+                    param * T(expressionPcaBasis(vertex_idx + 2, i)),
+                    T(0)
+            );
+        }
+
+        Eigen::Matrix<T, 4, 1> transformedVertex = transformationMatrix.cast<T>() * offset;
+
+        residuals[0] = transformedVertex.x() - T(m_landmark_image.x());
+        residuals[1] = transformedVertex.y() - T(m_landmark_image.y());
+        residuals[2] = transformedVertex.z() - T(m_landmark_image.z());
+
+        return true;
+    }
+
+private:
+    BaselFaceModel* m_baselFaceModel;
+    Vector3d m_landmark_image;
+    int m_landmark_bfm_index;
 };
 
 
