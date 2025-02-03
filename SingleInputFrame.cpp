f95fbc1899f71cb6953c30453a061cb4e17f20b2
Leo Keber <leo.keber@tum.de>
2025-02-03 13:10:58 +0100

    SingleInputFrame Initialization

--- SingleInputFrame.cpp
+++ SingleInputFrame.cpp
@@ -1,5 +1,12 @@
-//
-// Created by Leo on 03.02.2025.
-//
-
 #include "SingleInputFrame.h"
+
+#include <utility>
+
+SingleInputFrame::SingleInputFrame(std::vector<Vector3d> rgbData, std::vector<double> depthData,
+                                   std::vector<Vector3d> landmarks) {
+    m_rgb_data = std::move(rgbData);
+    m_depth_data = std::move(depthData);
+    m_landmarks = std::move(landmarks);
+}
+
+SingleInputFrame::~SingleInputFrame() = default;
--- SingleInputFrame.h
+++ SingleInputFrame.h
@@ -1,9 +1,16 @@
 #ifndef FACE_RECONSTRUCTION_SINGLEINPUTFRAME_H
 #define FACE_RECONSTRUCTION_SINGLEINPUTFRAME_H
 
+#include "Eigen.h"
 
 class SingleInputFrame {
-
+public:
+    SingleInputFrame(std::vector<Vector3d> rgbData, std::vector<double> depthData, std::vector<Vector3d> landmarks);
+    ~SingleInputFrame();
+private:
+    std::vector<Vector3d> m_rgb_data;
+    std::vector<double> m_depth_data;
+    std::vector<Vector3d> m_landmarks;
 };
 
 
