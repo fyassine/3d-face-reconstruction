3efeac5f2348bdca16ff65a3619dc70d9ecd80bb
Leo Keber <leo.keber@tum.de>
2025-02-03 13:22:42 +0100

    InputData Initialization

--- InputData.cpp
+++ InputData.cpp
@@ -1,5 +1,22 @@
-//
-// Created by Leo on 03.02.2025.
-//
-
 #include "InputData.h"
+
+#include <utility>
+
+InputData::InputData(std::vector<SingleInputFrame> frames, int width, int height, Matrix3d intrinsic_matrix,
+                     Matrix4d extrinsic_matrix, const SingleInputFrame& currentFrame) : m_currentFrame(currentFrame) {
+    m_frames = std::move(frames);
+    m_width = width;
+    m_height = height;
+    m_intrinsic_matrix = std::move(intrinsic_matrix);
+    m_extrinsic_matrix = std::move(extrinsic_matrix);
+}
+
+InputData::~InputData() = default;
+
+SingleInputFrame* InputData::processNextFrame() {
+    if(m_current_frame_index >= m_frames.size()){
+        return nullptr;
+    }
+    m_current_frame_index++;
+    return &m_frames[m_current_frame_index - 1];
+}
--- InputData.h
+++ InputData.h
@@ -1,10 +1,25 @@
 #ifndef FACE_RECONSTRUCTION_INPUTDATA_H
 #define FACE_RECONSTRUCTION_INPUTDATA_H
 
+#include "SingleInputFrame.h"
+#include <vector>
 
 class InputData {
+public:
+    InputData(std::vector<SingleInputFrame> frames, int width, int height, Matrix3d intrinsic_matrix,
+              Matrix4d extrinsic_matrix, const SingleInputFrame& currentFrame);
 
+    ~InputData();
+    SingleInputFrame* processNextFrame();
+private:
+    int m_current_frame_index = 0;
+    SingleInputFrame m_currentFrame;
+    std::vector<SingleInputFrame> m_frames;
+
+    int m_width;
+    int m_height;
+    Matrix3d m_intrinsic_matrix;
+    Matrix4d m_extrinsic_matrix;
 };
-
 
 #endif //FACE_RECONSTRUCTION_INPUTDATA_H
