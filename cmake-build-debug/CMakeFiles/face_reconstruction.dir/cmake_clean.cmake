0eb6fcf0dd9b47794c39434704be02b974796a31
Fraj Yassine Lakhal <fyassine.lakhal@tum.de>
2025-02-19 10:57:24 +0100

    Push project to remote

---
+++ cmake-build-debug/CMakeFiles/face_reconstruction.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/face_reconstruction.dir/BaselFaceModel.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/BaselFaceModel.cpp.o.d"
+  "CMakeFiles/face_reconstruction.dir/FaceReconstructor.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/FaceReconstructor.cpp.o.d"
+  "CMakeFiles/face_reconstruction.dir/FileReader.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/FileReader.cpp.o.d"
+  "CMakeFiles/face_reconstruction.dir/FreeImageHelper.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/FreeImageHelper.cpp.o.d"
+  "CMakeFiles/face_reconstruction.dir/InputData.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/InputData.cpp.o.d"
+  "CMakeFiles/face_reconstruction.dir/InputDataExtractor.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/InputDataExtractor.cpp.o.d"
+  "CMakeFiles/face_reconstruction.dir/ModelConverter.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/ModelConverter.cpp.o.d"
+  "CMakeFiles/face_reconstruction.dir/Optimizer.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/Optimizer.cpp.o.d"
+  "CMakeFiles/face_reconstruction.dir/Renderer.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/Renderer.cpp.o.d"
+  "CMakeFiles/face_reconstruction.dir/SingleInputFrame.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/SingleInputFrame.cpp.o.d"
+  "CMakeFiles/face_reconstruction.dir/main.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/main.cpp.o.d"
+  "face_reconstruction"
+  "face_reconstruction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/face_reconstruction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
